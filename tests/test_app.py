5880f9be55bbdcbe5d37b30583581b4dd66c67bd
Irina <lizllinin@gmail.com>
2025-10-24 16:51:06 -0500

    Primer commit - proyecto base con tests

---
+++ tests/test_app.py
@@ -0,0 +1,17 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.testing = True
+    with app.test_client() as client:
+        yield client
+
+def test_home_status(client):
+    response = client.get('/')
+    assert response.status_code == 200
+
+def test_suma_route(client):
+    response = client.get('/suma/2/3')
+    data = response.get_json()
+    assert data["resultado"] == 5
